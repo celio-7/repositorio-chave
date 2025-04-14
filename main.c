8aa7e5783d43a6d797dd6e5f59ac48ad81274566
celio-7 <celiodlj@gmail.com>
2025-04-14 11:52:52 -0300

    master calc switch

--- main.c
+++ main.c
@@ -2,44 +2,46 @@
 
 int main() {
 	
-	int idade = 0;
+	float n1,n2;
+	char simb;
 
-	printf("----PROGRAMA ELEITORAL----\n\n");
+	printf("----MASTER CALC 2025 PRO----\n\n");
+	printf("+\t-\t*\t/\t\t\t\t\n\n");
 
-	printf("Digite sua Idade: ");
-	scanf("%d", &idade);
-
-	if (idade == 84) {
-		printf("\nGanha premio Premium com necessidade de votar");
-	} else {
-		if (idade == 45) {
-			printf("\nGanha premio TOP sem necessidade de votar");
-		} else {
-			if (idade < 0) {
-				printf("\nNao Nasceu");
-			} else {
-				if (idade <= 15) {
-					if (idade > 0) {
-						printf("\nNao vota");
-					}
-				}
-				if (idade >= 16) {
-					if (idade <= 17) {
-						printf("\nPara voce e opcional o voto");
-					}
-				}
-				if (idade >= 18) {
-					if (idade < 65) {
-						printf("\nVoce tem a Obrigatoriedade de Votar");
-					}
-				}
-				if (idade >= 65) {
-					printf("\nPara voce e opcional o voto");
-				}
+	printf("Digite seu N1: ");
+	scanf("%f",&n1);
+	
+	printf("Digite seu operador: ");
+	scanf(" %c",&simb);
+	
+	printf("Digite seu N2: ");
+	scanf("%f",&n2);
+	
+	switch(simb){
+		case '+':
+			printf("O resultado da sua soma e: %.1f\n",n1+n2);
+		break;
+		
+		case'-':
+			printf("O resultado da sua subtracao e: %.1f\n",n1-n2);
+		break;
+		
+		case '*':
+			printf("O resultado da sua multiplicacao e: %.1f\n",n1*n2);
+		break;
+		
+		case '/':
+			if(n2==0){
+				printf("Nao existe divisao por 0\n");
+			}else{
+				printf("O resultado da sua divisao e: %.1f\n",n1/n2);
 			}
-		}
+		break;
+		
+		default:
+			printf("Digite um operador existente\n");	
+					
 	}
-
 	return 0;
 }
 
