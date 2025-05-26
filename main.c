6d01f79d0fdc5d43a48ee5ed47fc7fccd4335e5d
celin <celiodlj@gmail.com>
2025-05-26 12:37:12 -0300

    Pneumoultramicroscopicossilicovulcanoconiótico

--- main.c
+++ main.c
@@ -1,47 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define tam 50
+
 
 int main() {
 	
-	float n1,n2;
-	char simb;
+	int i=0,tamanho1=0,tamanho2=0;
+	char name1[tam],name2[tam];
+	
+	printf("Digite o 1 nome: ");
+	scanf("%[^\n]s",&name1);
+	fflush(stdin);
+	
+	printf("Digite o 2 nome: ");
+	scanf("%[^\n]s",&name2);
+	fflush(stdin);
+	
+	printf("--------------------\n");
+	
+	//Pneumoultramicroscopicossilicovulcanoconiótico
+	
+	tamanho1=strlen(name1);
+	tamanho2=strlen(name2);
+	
+	if(tamanho1>tamanho2){
+		printf("Menor nome informado: %s\n",name2);
+		printf("--------------------\n");
+		printf("Tamanho do 1: %d\n",tamanho1);
+		printf("Tamanho do 2: %d\n",tamanho2);
+	}else if(tamanho2>tamanho1){
+		printf("Menor nome informado: %s\n",name1);
+		printf("--------------------\n");
+		printf("Tamanho do 1: %d\n",tamanho1);
+		printf("Tamanho do 2: %d\n",tamanho2);
+	}else if(tamanho1==tamanho2)
+		printf("Os dois nomes tem o mesmo tamanho!\n");
+		printf("--------------------\n");
+		printf("Tamanho do 1: %d\n",tamanho1);
+		printf("Tamanho do 2: %d\n",tamanho2);
 
-	printf("----MASTER CALC 2025 PRO----\n\n");
-	printf("+\t-\t*\t/\t\t\t\t\n\n");
+	
+/*	printf("--------------------\n");
+	
+	printf("Tamanho: %d\n",tamanho);
+	
+	printf("--------------------\n");
+	
+	printf("Nome digitado: ");
 
-	printf("Digite seu N1: ");
-	scanf("%f",&n1);
-	
-	printf("Digite seu operador: ");
-	scanf(" %c",&simb);
-	
-	printf("Digite seu N2: ");
-	scanf("%f",&n2);
-	
-	switch(simb){
-		case '+':
-			printf("O resultado da sua soma e: %.1f\n",n1+n2);
-		break;
-		
-		case'-':
-			printf("O resultado da sua subtracao e: %.1f\n",n1-n2);
-		break;
-		
-		case '*':
-			printf("O resultado da sua multiplicacao e: %.1f\n",n1*n2);
-		break;
-		
-		case '/':
-			if(n2==0){
-				printf("Nao existe divisao por 0\n");
-			}else{
-				printf("O resultado da sua divisao e: %.1f\n",n1/n2);
-			}
-		break;
-		
-		default:
-			printf("Digite um operador existente\n");	
-					
+	for(i=tamanho-1;i>=0;i--){
+		printf("%c",name[i]);
 	}
+	*/
 	return 0;
 }
-
